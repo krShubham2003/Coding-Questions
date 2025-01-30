c174ad584346aced271071d109c7927b39d3fd07
Shubham <shubhamthakurbhardwaj583@gmail.com>
2025-01-30 23:07:46 +0530

    next greater element

--- Stack/Next Greater Element.cpp
+++ Stack/Next Greater Element.cpp
@@ -56,3 +56,21 @@
         return ans;
     }
 };
+
+
+// BRUTE FORCE APPROACH IS ------
+
+
+// vector<int>ans(n,-1);
+// for(int i=0;i<n;i++)
+// {
+//     for(int j=i+1;j<n;j++)
+//     {
+//         if(arr[j]>arr[i])
+//         {
+//             ans[i]=arr[j];
+//             break;
+//         }
+//     }
+//     return ans;
+// }
